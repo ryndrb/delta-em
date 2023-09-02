c55f6d5b86d8a9b45b1b70b45173c7145ff87929
ryndrb <49576487+ryndrb@users.noreply.github.com>
2023-09-02 14:48:40 -0400

    scripting (upto zinia petal. woods)
    - minor map tweaks in petal. woods and route 104
    - added briney and peeko's route 104 first encounter
    - researcher and grunt event done
    - researcher now gives the exp. share
    - zinia event done

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -41,9 +41,9 @@
 #define FLAG_TEMP_1F     (TEMP_FLAGS_START + 0x1F)
 #define TEMP_FLAGS_END   FLAG_TEMP_1F
 
-#define FLAG_UNUSED_0x020    0x20 // Unused Flag
-#define FLAG_UNUSED_0x021    0x21 // Unused Flag
-#define FLAG_UNUSED_0x022    0x22 // Unused Flag
+#define FLAG_HIDE_ROUTE_104_MR_BRINEY_2     0x20
+#define FLAG_HIDE_ROUTE_104_WINGULL_2       0x21
+#define FLAG_HIDE_PETALBURG_WOODS_ZINIA     0x22
 #define FLAG_UNUSED_0x023    0x23 // Unused Flag
 #define FLAG_UNUSED_0x024    0x24 // Unused Flag
 #define FLAG_UNUSED_0x025    0x25 // Unused Flag
