6318174df2d25f0892db832489e7db6090f9fe1b
ryndrb <49576487+ryndrb@users.noreply.github.com>
2023-09-23 14:11:10 -0400

    Wally, Zinnia encounter theme
    - wally: https://www.youtube.com/watch?v=Jcow5HqhkzU
    - zinnia: https://www.youtube.com/watch?v=ou63OrTHrOg
    - sounds good enough

--- include/constants/songs.h
+++ include/constants/songs.h
@@ -544,6 +544,9 @@
 #define PH_NURSE_HELD               608
 #define PH_NURSE_SOLO               609
 
+#define MUS_ZINNIA_THEME            610
+#define MUS_WALLY_THEME             611
+
 #define MUS_ROUTE118                0x7FFF  // Map is split into 2 music sections. controlled by GetCurrLocationDefaultMusic().
 
 #define MUS_NONE                    0xFFFF
--- songs.mk
+++ songs.mk
@@ -1262,3 +1262,9 @@ $(MID_SUBDIR)/se_m_leer.s: %.s: %.mid
 
 $(MID_SUBDIR)/se_dex_search.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -v100 -P5
+
+$(MID_SUBDIR)/mus_zinnia_theme.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G191 -V078
+
+$(MID_SUBDIR)/mus_wally_theme.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G191 -V078
