07e7f58f1608684e98101214af1d8552f12e00e6
ryndrb <49576487+ryndrb@users.noreply.github.com>
2023-10-09 20:36:58 -0400

    admin front pics
    - shelly: https://imgur.com/QB88XSN
    - matt: https://www.deviantart.com/icejkai/art/Aqua-Admin-Matt-Trainer-Sprite-650920229
    - tabitha: https://www.deviantart.com/justin8964/art/ORAS-Tabitha-Sprite-969833909
    - courtney: https://www.deviantart.com/icejkai/art/Magma-Admin-Courtney-Trainer-Sprite-544510574

--- include/graphics.h
+++ include/graphics.h
@@ -7336,6 +7336,7 @@ extern const u32 gTrainerFrontPic_Red[];
 extern const u32 gTrainerFrontPic_Leaf[];
 extern const u32 gTrainerFrontPic_RubySapphireBrendan[];
 extern const u32 gTrainerFrontPic_RubySapphireMay[];
+extern const u32 gTrainerFrontPic_MagmaAdminF[];
 
 extern const u32 gTrainerPalette_Hiker[];
 extern const u32 gTrainerPalette_AquaGruntM[];
@@ -7430,6 +7431,7 @@ extern const u32 gTrainerPalette_Red[];
 extern const u32 gTrainerPalette_Leaf[];
 extern const u32 gTrainerPalette_RubySapphireBrendan[];
 extern const u32 gTrainerPalette_RubySapphireMay[];
+extern const u32 gTrainerPalette_MagmaAdminF[];
 
 extern const u8 gTrainerBackPic_Brendan[];
 extern const u8 gTrainerBackPic_May[];
